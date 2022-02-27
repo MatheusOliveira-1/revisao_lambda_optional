7ed739309545c31a2a901dc77fd3bafe2a179fe0
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-02-27 17:59:18 -0300

    testados .empty, .of, .ofNullAbel e orElse

--- src/application/EuAchoEPouco.java
+++ src/application/EuAchoEPouco.java
@@ -5,10 +5,33 @@ import java.util.Optional;
 public class EuAchoEPouco {
 	public static void main(String [] args) {
 		
-		Optional <Object> bloco = Optional.empty();
+		Optional <String> blocovazio = Optional.empty();//O optional 'empty' deve estar sempre vazio
 		
-		System.out.println(bloco.isPresent());
-		System.out.println(bloco.isEmpty());
+		System.out.println(":::::::::::TESTANDO OPTIONAL EMPTY:::::::::::");
+		System.out.println("isPresent: " + blocovazio.isPresent());
+		System.out.println("isEmpty: " + blocovazio.isEmpty());
+		
+		Optional <String> blococheio = Optional.of("\nGeneration Largo da Batata");//O optional 'of' deve sempre conter alguma informação
+		
+		System.out.println("\n:::::::::::TESTANDO OPTIONAL OF:::::::::::");
+		System.out.println("isPresent: " + blococheio.isPresent());
+		System.out.println("isEmpty: " + blococheio.isEmpty());
+		
+		String resposta = blococheio.orElse("Mas eu só tenho 6 anos");
+		
+		System.out.println(resposta);
+		
+		Optional <String> blococheioorelse = Optional.ofNullable(null);//O optional 'of' deve sempre conter alguma informação
+		
+		System.out.println("\n:::::::::::TESTANDO OFNULLABLE E ORELSE:::::::::::");
+		System.out.println("isPresent: " + blococheioorelse.isPresent());
+		System.out.println("isEmpty: " + blococheioorelse.isEmpty());
+		
+		String resposta2 = blococheioorelse.orElse("\nMas eu só tenho 6 anos");
+		
+		System.out.println(resposta2);
+		
+		
 	}
 
 }
